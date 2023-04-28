915a4ada3e0fcb33ad103e2991a3512e7b2b290b
unknown <pqdaniela@hotmail.com>
2023-04-28 20:02:31 +0100

    nova atualização

--- MatrizQubo.py
+++ MatrizQubo.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from tabu import TabuSampler
 
 # Matriz de distâncias
 d = np.array([[0, 23, 23, 24],
@@ -14,25 +15,23 @@ def tradutor(row, col):
     return ind_matriz
 
 # Criar o primeiro QUBO
-Q1 = np.zeros((17, 17))
-
+Q1 = np.zeros((16, 16))
 for i in range(n):
     for j in range(n):
-        for t in range(n):
+        for t in range(n-1):
             linha = tradutor(t, i)
-            if t == n-1: #para considerar que quando t=3 então t+1=0
-                coluna = tradutor(0, j)
-            else:
-                coluna = tradutor(t+1, j)
-            Q1[linha][coluna] = d[i][j]
+            coluna = tradutor(t+1, j)
+            Q1[linha][coluna] = d[i][j]/2
+            Q1[coluna][linha] = d[i][j]/2
 
 df = pd.DataFrame(Q1)
 df.to_excel("Q1.xlsx")
 
+
+
 # criar o segundo Qubo
-Q2 = np.zeros((17, 17))
-N=len(Q2)
-P2= 100
+Q2 = np.zeros((16, 16))
+P2= 110
 for i in range(n):
     for t in range(n):
         linha = tradutor(t, i)
@@ -41,17 +40,17 @@ for i in range(n):
             if t == tt:
                 Q2[linha][coluna] -= 1
             else:
-                Q2[linha][coluna] += 2
-        
-Q2[N-1][N-1]=1
+                Q2[linha][coluna] += 2/2
+
+
 Q2 = P2*Q2
 df2 = pd.DataFrame(Q2)
 df2.to_excel("Q2.xlsx")
 
 
 # Criar o terceiro QUBO
-Q3 = np.zeros((17, 17))
-P3=100
+Q3 = np.zeros((16, 16))
+P3=110
 for t in range(n):
     for i in range(n):
         linha = tradutor(t, i)
@@ -60,17 +59,28 @@ for t in range(n):
             if i == j:
                 Q3[linha][coluna] -= 1
             else:
-                Q3[linha][coluna] += 2
-        
-Q3[N-1][N-1]=1
+                Q3[linha][coluna] += 2/2
+
 Q3= P3*Q3
+df3 = pd.DataFrame(Q3)
+df3.to_excel("Q3.xlsx")
+
 
 Q= Q1+Q2+Q3
 
-from tabu import TabuSampler
-sampler = TabuSampler()
-response = sampler.sample_qubo(Q)
 
+b = Q.transpose()
+# Comparing both arrays using array_equal() method
+if np.array_equal(Q, b):
+    print("The array is Symmetric")
+else:
+    print("The array is Not Symmetric")
+
+
+
+sampler = TabuSampler()
+response = sampler.sample_qubo()
+print(response)
 # Encontrar a solução com menor energia
 sample = response.first.sample
 
